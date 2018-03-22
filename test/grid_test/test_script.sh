86622dc429b40f1d5a905d436c55e53d8c8fcfb5
Andrea Anzilotti <a.anzilotti@hastega.it>
2018-03-22 17:32:30 +0100

    added tests

--- test/grid_test/test_script.sh
+++ test/grid_test/test_script.sh
@@ -1,1 +1,4 @@
-jpgtn -G -J 800 -K 600 -R 2 -c 3 -N test_results.jpg ./minions
+jpgtn -G -J 800 -K 600 -R 2 -C 3 -N test_results_1.jpg ./minions
+jpgtn -G -J 800 -K 600 -R 3 -C 2 -N test_results_2.jpg ./minions
+jpgtn -G -J 400 -K 600 -R 2 -C 3 -N test_results_3.jpg ./minions
+jpgtn -G -J 100 -K 200 -R 1 -C 2 -N test_results_4.jpg ./minions
